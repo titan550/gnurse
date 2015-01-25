0752f9c42273d7e28d7d00d548c34acfcbe3522e
Reza Khan <titan.550@gmail.com>
2015-01-24 16:11:28 -0800

    First Commit

---
+++ app/src/main/java/com/kairez/android/greatnurse/LoginFragment.java
@@ -0,0 +1,24 @@
+package com.kairez.android.greatnurse;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LoginFragment extends Fragment
+{
+
+    public LoginFragment()
+    {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_login, container, false);
+        return rootView;
+    }
+}
